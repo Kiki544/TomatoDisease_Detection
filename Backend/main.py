8cfed4b8fdb6e43bc162c84fb505731a7de102c3
Kiki544 <aboderjohn89@gmail.com>
2025-08-18 09:09:31 +0100

    Frontend built for deployment

--- Backend/main.py
+++ Backend/main.py
@@ -1,15 +1,14 @@
 import os
+import uvicorn
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from predict import predict
-import uvicorn
 
 app = FastAPI()
 
-# CORS settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow all origins
+    allow_origins=["*"],  # allow frontend requests
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -17,18 +16,13 @@ app.add_middleware(
 
 @app.post("/predict")
 async def classify_image(file: UploadFile = File(...)):
-    print("Request received")
-    print("Filename:", file.filename)
     image_bytes = await file.read()
     try:
-        result = predict(image_bytes)
-        print("Prediction result:", result)
-        return result
+        return predict(image_bytes)
     except Exception as e:
-        print("Error:", e)
         return {"error": str(e)}
 
-# Start Uvicorn with Render's dynamic port
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Render assigns PORT dynamically
+    # Use Render's dynamic port, default to 8000 locally
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
