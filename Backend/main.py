7fedf61c8b8d5ccadd65a34f1a9e96dc38de6bf9
Kiki544 <aboderjohn89@gmail.com>
2025-08-18 09:19:50 +0100

    Frontend built for deployment

--- Backend/main.py
+++ Backend/main.py
@@ -25,4 +25,4 @@ async def classify_image(file: UploadFile = File(...)):
 if __name__ == "__main__":
     # Use Render's dynamic port, default to 8000 locally
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
